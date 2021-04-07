4af96e18e3865af896a0c6bfd850e4367ab7b51b
harshith9999 <harshith9999@users.noreply.github.com>
2021-04-07 19:35:39 +0530

    color changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 import { Nav, NavDropdown, Navbar } from "react-bootstrap";
 import "./Navbar.css";
-import logo from './logo.png'
+import logo from "./logo.png";
 
 function NavigationBar() {
-  const [clicked, setclicked] = useState(false);
-
-  const handleClick = () => {
-    setclicked(!clicked);
-  };
   return (
-    <Navbar expand="lg">
-      <Navbar.Brand href="#home"><img style={{width: 150, height: 70}} src={logo}/></Navbar.Brand>
+    <Navbar expand="lg" variant="dark">
+      <Navbar.Brand href="#home">
+        <img style={{ width: 150, height: 70 }} src={logo} />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/about">About us</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
+
           <NavDropdown title="Courses" id="basic-nav-dropdown">
             <NavDropdown.Item>Civil Engineering</NavDropdown.Item>
             <NavDropdown.Item>Mechanical Engineering</NavDropdown.Item>
